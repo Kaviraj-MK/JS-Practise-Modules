4ba631f9dadd4bfb9890da44f5831b3ac959b480
Kaviraj-MK <73822551+Kaviraj-MK@users.noreply.github.com>
2022-07-06 22:03:21 +0530

    Add files via upload

---
+++ serverBasic&fetch/15createServer2.js
@@ -0,0 +1,97 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { json } = require('stream/consumers');
+
+
+const port = 3000;
+const hostName = '127.0.0.1';
+
+const getMimeType = (filePath) => {
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        'jpg' : 'image/jpg'
+    };
+    return mimeTypes[extname] || 'application/octet-stream';
+}
+
+/*const server = http.createServer((req, res) => {
+    fs.readFile('Test1.html', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.end({ 'error': 'Error Occured.' });
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data, 'utf-8');
+    });
+});*/
+const preparePath = (url) => {
+    let filePath = '.' + url;
+    if (filePath == './') {
+        filePath = './Test1.html';
+    }
+    return filePath;
+}
+
+const server = http.createServer((req, res) => {
+
+    const filePath = preparePath(req.url);
+
+    if(filePath.includes('/api/')){ //check whether this is a API call from myScript
+        console.log('Gnerating api result.');
+        if(filePath.includes('data')){
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            return res.end(JSON.stringify({data: 'Hi'}));
+        }
+        if(filePath.includes('studentInfo')){
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            return res.end(JSON.stringify({
+                Fisrt_Name : 'Selwamuththu',
+                Middle_Name : 'Kandasami',
+                Address : 'Galle' 
+            }));
+        }
+    }
+
+    if(filePath === './favicon.ico') {
+        res.writeHead(404, {'Content-Type' : 'image/x-icon'});
+        return res.end();
+    }
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error('Error Server: ', err);
+            res.writeHead(404);
+            res.end({ 'error': 'Error Occured.' });
+        }
+        res.writeHead(200, { 'Content-Type': getMimeType(filePath) });
+        res.end(data, 'utf-8');
+    });
+});
+
+// const contentType = getMimeType(filePath);
+// fs.readFile(filePath, function(error, content) {
+//     if (error) {
+//         if(error.code == 'ENOENT') {
+//             fs.readFile('./404.html', function(error, content) {
+//                 res.writeHead(404, { 'Content-Type': 'text/html' });
+//                 res.end(content, 'utf-8');
+//             });
+//         }
+//         else {
+//             res.writeHead(500);
+//             res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+//         }
+//     }
+//     else {
+//         console.log('file path', filePath);
+//         res.writeHead(200, { 'Content-Type': contentType });
+//         res.end(content, 'utf-8');
+//     }
+// });.
+
+server.listen(port, hostName, () => {
+    console.log(`Server2 runninng at http://${hostName}:${port}/`);
+}); 
